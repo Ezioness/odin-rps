3512a5d1daa48ecc59756cc7a6c7c55e2f19ba92
Ezioness <romcas@gmx.fr>
2024-07-21 18:25:32 +0200

    Computer choice added.
    Added the functionnality to choose between the 3 rps moves.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,26 @@
 console.log("Hello, world!")
+
+let getComputerChoice = () => {
+    /*
+        get a random number from 0 to 2
+        use a switch statement to check that number
+        return the move choosen from the switch statement
+    */
+    const randomNumber = Math.floor(Math.random() * 3)
+    let move = ""
+    switch(randomNumber) {
+        case 0:
+            move = "rock"
+            break
+        case 1:
+            move = "paper"
+            break
+        case 2:
+            move = "scissors"
+            break
+    }
+
+    return move
+}
+
+console.log(getComputerChoice())
